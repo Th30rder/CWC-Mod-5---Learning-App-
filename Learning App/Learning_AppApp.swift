24f51354af68d9fcb2ce0d17235f478f70e0f41b
Logan M Partin <logapart17@bossierstudents.org>
2021-11-30 12:48:15 -0600

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by PM C - Logan Partin  on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
