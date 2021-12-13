97977bc9ed329156389c02acceff25a52d70b22b
Logan M Partin <logapart17@bossierstudents.org>
2021-12-13 12:16:44 -0600

    Code for Mod 5 - L5 - but jsonUrl is still nil.

---
+++ Learning App/Views/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Learning App
+//
+//  Created by PM C - Logan Partin  on 12/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            LazyVStack {
+                
+                // Confirm that currentModule is set
+                if model.currentModule != nil {
+                
+                    ForEach(0..<model.currentModule!.content.lessons.count) { index in
+                        
+                        ContentViewRow(index: index)
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Learn \(model.currentModule?.category ?? "")")
+            
+        }
+        
+        
+    }
+}
